97a1e52215514473e2d13c41ab5419e2b371a757
Fluttershy <reality-inspired@hotmail.com>
2020-08-25 18:27:12 -0400

    Fixed base path issue

---
+++ react/icecube/precache-manifest.adf30265b6693c1734e664edfa34bb99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05fca50437b2bc9a7cd127960685936",
+    "url": "/react/icecube/index.html"
+  },
+  {
+    "revision": "a035ff75e12cb4e42929",
+    "url": "/react/icecube/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "3b251703fea44c906977",
+    "url": "/react/icecube/static/js/2.c670af94.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/react/icecube/static/js/2.c670af94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a035ff75e12cb4e42929",
+    "url": "/react/icecube/static/js/main.6bb53ef8.chunk.js"
+  },
+  {
+    "revision": "0c932971fe7e42daeae5",
+    "url": "/react/icecube/static/js/runtime-main.6af608f7.js"
+  }
+]);
--- react/icecube/service-worker.js
+++ react/icecube/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2655a6e19d51068cda1f1898d786682.js"
+  "/react/icecube/precache-manifest.adf30265b6693c1734e664edfa34bb99.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/icecube/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
