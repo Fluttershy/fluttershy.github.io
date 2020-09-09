e81da6516c72c3195109a5cb7c33005ef69248d4
Fluttershy <reality-inspired@hotmail.com>
2020-09-09 16:23:15 -0400

    Initial draft of react/rubix

---
+++ react/rubix/precache-manifest.f22c554c95f6d65f09bd5a20b2488c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63be89f0c373651332483224b3b0dda2",
+    "url": "/react/rubix/index.html"
+  },
+  {
+    "revision": "9f5b28940cc8cd79fd83",
+    "url": "/react/rubix/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "339162e8c27c7978c844",
+    "url": "/react/rubix/static/js/2.cd31748c.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/react/rubix/static/js/2.cd31748c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5b28940cc8cd79fd83",
+    "url": "/react/rubix/static/js/main.2d7e55f6.chunk.js"
+  },
+  {
+    "revision": "60754519f9e8535fc6ce",
+    "url": "/react/rubix/static/js/runtime-main.505d02c0.js"
+  }
+]);
