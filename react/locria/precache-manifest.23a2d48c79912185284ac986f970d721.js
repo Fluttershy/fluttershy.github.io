7981b53ea9b5aab9dcb5585bdabb2efe8f7ed06e
Fluttershy <reality-inspired@hotmail.com>
2020-09-11 12:16:30 -0400

    fixed homepage for react/locria

---
+++ react/locria/precache-manifest.23a2d48c79912185284ac986f970d721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2264f36bcda44ea4dd7a86a813e6fe",
+    "url": "/react/locria/index.html"
+  },
+  {
+    "revision": "21b759d8f20343189d7b",
+    "url": "/react/locria/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "84a3ec851b1be669920f",
+    "url": "/react/locria/static/js/2.34ae5aa8.chunk.js"
+  },
+  {
+    "revision": "1bec314975a2e0a2a324d29fe4a8423d",
+    "url": "/react/locria/static/js/2.34ae5aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b759d8f20343189d7b",
+    "url": "/react/locria/static/js/main.acfd10c1.chunk.js"
+  },
+  {
+    "revision": "5195a84e85ac4b272c9c",
+    "url": "/react/locria/static/js/runtime-main.8d1112fe.js"
+  }
+]);
