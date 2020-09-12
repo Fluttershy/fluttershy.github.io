8bf8ec5b73cec3dfdc1e44366afceaee144c5919
Fluttershy <reality-inspired@hotmail.com>
2020-09-12 03:31:55 -0400

    testing homepage ./ for react/locria

--- react/locria/service-worker.js
+++ react/locria/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react/locria/precache-manifest.23a2d48c79912185284ac986f970d721.js"
+  "./precache-manifest.db5b3d3a757bfa4a697a24f8e1552a54.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/locria/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
